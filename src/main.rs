c3c7dd6615acbd60c21a5322549b773e83a609d2
Devon Scott <DevonJScott@hotmail.com>
2026-09-01 12:58:26 -0700

    Verification implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 type Board = [[i16;9]; 9];
 
-const TEST_GRID: [[i16; 9]; 9] = [
+const TEST_GRID: Board = [
     [0, 0, 0, 0, 0, 8, 1, 0, 0],
     [0, 0, 0, 0, 0, 0, 9, 0, 0],
     [3, 0, 4, 9, 0, 0, 8, 2, 0],
@@ -11,12 +11,73 @@ const TEST_GRID: [[i16; 9]; 9] = [
     [0, 6, 1, 0, 8, 3, 0, 4, 0],
     [4, 0, 0, 5, 0, 0, 0, 6, 0]]; 
 
-fn verify(board: &Board) -> bool {
-    for row in 1..10 {
-        let mut checks: [bool; 9] = [false; 9];
+const SOLVED_GRID: Board = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8]
+];
 
+fn verify(board: &Board) -> bool {
+    for row in 0..9 {
+        let mut row_checks: [bool; 9] = [false; 9];
+        for col in 0..9 {
+            let num: usize = board[row][col] as usize;
+            if num == 0 {
+                return false
+            }
+            row_checks[num - 1] = true
+        }
+        for i in 0..9{
+            if row_checks[i] == false {
+                return false
+            }
+        }
     }
-    false
+
+    for col in 0..9 {
+        let mut col_checks: [bool; 9] = [false; 9];
+        for row in 0..9 {
+            let num: usize = board[row][col] as usize;
+            if num == 0 {
+                return false
+            }
+            col_checks[num - 1] = true
+        }
+        for i in 0..9{
+            if col_checks[i] == false {
+                return false
+            }
+        }
+    }
+
+    for box_row in 0..3 {
+        for box_col in 0..3 {
+            let mut box_checks = [false; 9];
+            for row in 0..3 {
+                for col in 0..3 {
+                    let r: usize = box_row * 3 + row;
+                    let c: usize = box_col * 3 + col;
+                    let num: usize = board[r][c] as usize;
+                    if num == 0 {
+                        return false
+                    }
+                    box_checks[num - 1] = true;
+                }
+            }
+            for i in 0..9{
+                if box_checks[i] == false {
+                    return false
+                }
+            }
+        }
+    }
+    true
 }
 
 
@@ -28,5 +89,11 @@ fn check_rows_cols(board: &mut Board) -> bool {
 }
 
 fn main() {
-    let mut _board: Board = TEST_GRID;
+    let board: Board = TEST_GRID;
+    if !verify(&board) {
+        print!("Board is not solved\n")
+    }
+    if verify(&SOLVED_GRID) {
+        print!("Solved board is solved\n")
+    }
 }
