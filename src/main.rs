7c11b098ae2160384e65a953499694a753efcb96
Devon Scott <devonjscott@hotmail.com>
2026-09-01 12:17:17 -0700

    Initial framework

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+type Board = [[i16;9]; 9];
+
+const TEST_GRID: [[i16; 9]; 9] = [
+    [0, 0, 0, 0, 0, 8, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 9, 0, 0],
+    [3, 0, 4, 9, 0, 0, 8, 2, 0],
+    [7, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 9, 0, 1, 6, 0, 7, 0],
+    [0, 0, 3, 0, 6, 0, 0, 0, 1],
+    [0, 6, 1, 0, 8, 3, 0, 4, 0],
+    [4, 0, 0, 5, 0, 0, 0, 6, 0]]; 
+
+fn verify(board: &Board) -> bool {
+    for row in 1..10 {
+        let mut checks: [bool; 9] = [false; 9];
+
+    }
+    false
+}
+
+
+fn check_rows_cols(board: &mut Board) -> bool {
+    for n in 1..10 {
+        
+    };
+    false
+}
+
+fn main() {
+    let mut _board: Board = TEST_GRID;
+}
