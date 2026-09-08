911aca401d9ef5d64e3467334109055e663a483e
Devon Scott <DevonJScott@hotmail.com>
2026-09-08 10:53:34 -0700

    Adding setter helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -48,6 +48,89 @@ pub fn get_unit_candidate_masks(board: &Board, row: usize, col: usize, mode: Uni
     candidate_set
 }
 
-fn set_unit_candidate_masks(board: &mut Board, index: usize, masks: &Vec<(usize, BitMask)>, mode: UnitMode) {
-    
+pub fn set_unit_candidate_masks(board: &mut Board, index: usize, masks: &[(usize, BitMask)], mode: UnitMode) -> bool {
+    let mut change = false;
+    match mode {
+        UnitMode::Row => {
+            for (col, mask) in masks {
+                if let Cell::Candidates(bits) = &mut board[index][*col] {
+                    if bits != mask {
+                        board[index][*col] = Cell::Candidates(*mask);
+                        change = true;
+                    }
+                }
+                else {
+                    panic!("Row Candidate indexes did not match a call to get_unit_candidate_masks")
+                }
+            }
+        }
+        UnitMode::Column => {
+            for (row, mask) in masks {
+                if let Cell::Candidates(bits) = &mut board[*row][index] {
+                    if bits != mask {
+                        board[*row][index] = Cell::Candidates(*mask);
+                        change = true;
+                    }
+                }
+                else {
+                    panic!("Column Candidate indexes did not match a call to get_unit_candidate_masks")
+                }
+            }
+        }
+        UnitMode::Box => {
+            // Truncated division: e.g. row in [3, 4, 5] -> r = 3
+            let row_start = (index / 3) * 3;
+            let col_start = (index % 3) * 3;
+            for (idx, mask) in masks {
+                let row = row_start + (idx / 3);
+                let col = col_start + (idx % 3);
+                if let Cell::Candidates(bits) = &mut board[row][col] {
+                    if bits != mask {
+                        board[row][col] = Cell::Candidates(*mask);
+                        change = true;
+                    }
+                }
+                else {
+                    panic!("Box Candidate indexes did not match a call to get_unit_candidate_masks")
+                }
+            }
+        }
+    }
+    change
+}
+
+pub fn set_unit(board: &mut Board, index: usize, masks: &[Cell], mode: UnitMode) -> bool {
+    let mut change = false;
+    match mode {
+        UnitMode::Row => {
+            for col in 0..9 {
+                if board[index][col] != masks[col] {
+                    board[index][col] = masks[col];
+                    change = true;
+                }
+            }
+        }
+        UnitMode::Column => {
+            for row in 0..9 {
+                if board[row][index] != masks[row] {
+                    board[row][index] = masks[row];
+                    change = true;
+                }
+            }
+        }
+        UnitMode::Box => {
+            // Truncated division: e.g. row in [3, 4, 5] -> r = 3
+            let row_start = (index / 3) * 3;
+            let col_start = (index % 3) * 3;
+            for idx in 0..9 {
+                let row = row_start + (idx / 3);
+                let col = col_start + (idx % 3);
+                if board[row][col] != masks[idx] {
+                    board[row][col] = masks[idx];
+                    change = true;
+                }
+            }
+        }
+    }
+    change
 }
